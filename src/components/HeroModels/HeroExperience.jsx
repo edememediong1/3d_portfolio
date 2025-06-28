59bc327a927c6d71d158a54eebd3d3caba1ca308
Emediong Bassey Edem <edememediong1@gmail.com>
2025-06-28 23:11:21 +0100

    chore:

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -11,7 +11,6 @@ const HeroExperience = () => {
     const isMobile = useMediaQuery({query : '(max-width : 768px)'});
   return (
     <Canvas camera={{position: [0, 0, 15], fov: 45}}>
-
         <HeroLights/>
         <Particles
           count={100} 
