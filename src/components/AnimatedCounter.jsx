44ee4b5bf106b360940fc96ad98df342eeffbf4a
Emediong Bassey Edem <edememediong1@gmail.com>
2025-07-01 21:42:49 +0100

    feat: added the Animate counter section

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { counterItems } from '../constants'
+
 
 const AnimatedCounter = () => {
   return (
-    <div>AnimatedCounter</div>
+    <div id='counter' className='padding-x-lg xl:mt-0 mt-32'>
+      <div className='mx-auto grid-4-cols'>
+        {counterItems.map((item) => (
+          <div key={item.label} className='counter-number text-white text-5xl font-bold mb-2'>
+            0 {item.suffix}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Button from '../components/Button'
 import HeroExperience from '../components/HeroModels/HeroExperience'
 import {useGSAP} from '@gsap/react'
 import gsap  from 'gsap'
-
+import AnimatedCounter from '../components/AnimatedCounter'
 
 const Hero = () => {
     useGSAP(() => {
@@ -70,6 +70,7 @@ const Hero = () => {
                 </div>
             </figure>
         </div>
+        <AnimatedCounter />
     </section>
   )
 }
