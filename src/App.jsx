f55b87f18df82fb9d39efae5c3c98abe4643e612
Emediong Bassey Edem <edememediong1@gmail.com>
2025-07-31 01:22:56 +0100

    feat: created the TechStacks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar'
 import LogoSection from './sections/LogoSection'
 import FeatureCards from './sections/FeatureCards'
 import ExperienceSection from './sections/ExperienceSection'
-
+import TechStack from './sections/TechStack'
 
 const App = () => {
   return (
@@ -16,6 +16,7 @@ const App = () => {
       <LogoSection/>
       <FeatureCards/>
       <ExperienceSection/>
+      <TechStack />
     </>
   )
 }
--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -24,7 +24,34 @@ const ExperienceSection = () => {
             })
         })
 
-        gsap.to()
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% center',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress
+                    })
+                }
+            }
+        })
+
+
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, {
+                xPercent: 0,
+                opacity: 0,
+                duration: 1,
+                ease: 'power2.inOut',
+                scrollTrigger: {
+                    trigger: text,
+                    start: 'top 60%'
+                }
+            })
+        })
     }, [])
   return (
     <section id="experience" className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
