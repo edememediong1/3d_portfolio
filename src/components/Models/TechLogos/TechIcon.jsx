07f20d634d9e6ebacb176e41c6886146f142acb9
Emediong Bassey Edem <edememediong1@gmail.com>
2025-08-03 23:54:29 +0100

    added techIcon

---
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TechIcon() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TechIcon
--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
-
+import TechIcon from '../components/Models/TechLogos/TechIcon'
 
 export default function TechStack() {
   return (
